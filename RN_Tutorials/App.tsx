61a5e7124d6420320af9d81f84ac71371f40482f
Srikanth Vallepu <srikanth.vallepu@yesgnome.com>
2023-07-10 23:02:41 +0530

    list view

--- RN_Tutorials/App.tsx
+++ RN_Tutorials/App.tsx
@@ -1,41 +1,71 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 
-const StateDemo = () => {
-  const [myTitle, setMytitle] = useState('React Native');
+const App = () => {
+  const [myList, setMylist] = useState([
+    {id: 0, title: 'ReactNative'},
+    {id: 1, title: 'ReactJS'},
+    {id: 2, title: 'Node'},
+    {id: 3, title: 'Java'},
+    {id: 4, title: 'orcle'},
+    {id: 5, title: '.Net'},
+    {id: 6, title: 'ReactNative'},
+    {id: 7, title: 'ReactJS'},
+    {id: 8, title: 'Node'},
+    {id: 9, title: 'Java'},
+    {id: 10, title: 'orcle'},
+    {id: 11, title: '.Net'},
+    {id: 12, title: 'ReactNative'},
+    {id: 13, title: 'ReactJS'},
+    {id: 14, title: 'Node'},
+    {id: 15, title: 'Java'},
+    {id: 16, title: 'orcle'},
+    {id: 17, title: '.Net'},
+    {id: 18, title: 'ReactNative'},
+    {id: 19, title: 'ReactJS'},
+    {id: 20, title: 'Node'},
+    {id: 21, title: 'Java'},
+    {id: 22, title: 'orcle'},
+    {id: 23, title: '.Net'},
+  ]);
 
-  const ChangeTitle1 = () => {
-    setMytitle('ReactJS');
+  const _renderItem = ({item, index}: any) => {
+    return (
+      <View
+        style={{
+          padding: 10,
+          marginTop: 3,
+          backgroundColor: 'lightgreen',
+          alignItems: 'center',
+        }}>
+        <Text>{item.title}</Text>
+      </View>
+    );
   };
-
-  const ChangeTitle2 = () => {
-    setMytitle('React Native');
-  };
-
-  //presentational component
-  const ShowTitle = props => {
-    return <Text style={{fontSize: 30}}>{props.title}</Text>;
-  };
-
-  //container component
+  //container componenit
   return (
     <View
       style={{
-        flex: 1,
-        alignItems: 'center',
         backgroundColor: 'lightyellow',
-        justifyContent: 'center',
       }}>
-      <Text style={{fontSize: 50}}>Title</Text>
+      <Text style={{fontSize: 50}}>List View</Text>
+      {/* {myList.map(item => {
+        return (
+          <View
+            style={{
+              padding: 10,
+              marginTop: 3,
+              backgroundColor: 'lightgreen',
+              alignItems: 'center',
+            }}>
+            <Text>{item.title}</Text>
+          </View>
+        );
+      })} */}
 
-      {/* <Text style={{fontSize: 50}}>{myTitle}</Text> */}
-
-      <ShowTitle title="React Native" />
-
-      <Button title="change1" onPress={ChangeTitle1}></Button>
-      <Button title="change2" onPress={ChangeTitle2}></Button>
+      <FlatList data={myList} renderItem={_renderItem}></FlatList>
     </View>
   );
 };
-export default StateDemo;
+export default App;
